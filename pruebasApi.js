31bf58eb02cdad5743aa678159ea993c095aa939
WILLIAMCODCPRO <williamcodpro@gmail.com>
2025-08-22 11:44:29 -0500

    feat: :sparkles: add login estudiante

---
+++ pruebasApi.js
@@ -0,0 +1,29 @@
+let escojerUsuario = prompt("estudiate, profesor");
+
+if(escojerUsuario === "estudiante"){
+    let contrasena = prompt("contraseña");
+    let correo = prompt("correo");
+
+    let llamadaApiEstudiantes = new XMLHttpRequest();
+    llamadaApiEstudiantes.open("GET", "https://68a88e91a7e3ec06c2fc4249.mockapi.io/estudiantes/datosEstudiante")
+    llamadaApiEstudiantes.onreadystatechange = function(){
+        if(llamadaApiEstudiantes.readyState === 4 && llamadaApiEstudiantes.status === 200){
+            try{
+                let datosEstudiantes = JSON.parse(llamadaApiEstudiantes.responseText);
+                for(let i = 0; i < datosEstudiantes.length; i++){
+                    if (datosEstudiantes[i]["clave"] === contrasena && datosEstudiantes[i]["correo"] === correo){
+                        alert("Felicidades")
+                    }
+                    else alert("usuario no encontrado")
+                }
+            }
+
+            catch(err){
+
+                console.log(err.message);
+            }
+        }
+    }
+    llamadaApiEstudiantes.send();
+
+}
