ceb57958c7b24542eebd07a6a1f1911682b0a639
WILLIAMCODCPRO <williamcodcpro@gmail.com>
2025-08-23 11:45:29 -0500

    feta: :sparkles: improve user type selection and login redirection

--- scripts/login.js
+++ scripts/login.js
@@ -1,5 +1,16 @@
-function aparecerLogin(){
+const botonEstudiante = document.getElementById("botonEstudiante")
+const botonDocente = document.getElementById("botonDocente")
+
+botonEstudiante.addEventListener("click", function(){
     document.querySelector(".container").style.display = "none";
     document.querySelector(".container2").style.display = "flex";
+})
+    
+botonDocente.addEventListener("click", function(){
+    document.querySelector(".container").style.display = "none";
+    document.querySelector(".container2").style.display = "flex";
+})
+    
 
-}
+
+
